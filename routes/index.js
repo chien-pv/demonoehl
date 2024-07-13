b6ec1146b9879d2b8fa6742ad9874bb6c960212f
chienpv <phamvanbk11@gmail.com>
2024-07-13 14:47:53 +0700

    demo mysql

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
+//sử dung module database
+var mysql = require("mysql");
+
+// create connection to database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "demonodehl",
+});
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let dateNow = new Date();
@@ -23,7 +34,10 @@ router.get("/", function (req, res, next) {
         "Ken Mogi mang tới cho độc giả một nhận thức sâu sắc về ikigai, đồng thời hiểu thêm về lịch sử và văn hóa Nhật Bản. Những giá trị của ikigai mà bạn thu nhận từ sách có thể giúp bạn tìm ra ikigai của mình, và quan trọng hơn hết",
     },
   ];
-
+  let sql = "select * from products";
+  db.query(sql, function (err, data) {
+    console.log(data);
+  });
   res.render("index", { title: "Express", dateNow: dateNow, uname, products });
 });
 
